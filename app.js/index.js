a7676473acc439171a5d1ed1b21107ceaf3be415
BHObserver <BHObserver0@gmail.com>
2023-08-09 18:28:35 +0600

    Update index.js

--- app.js/index.js
+++ app.js/index.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-unused-vars */
 const colorItem = document.querySelector('body');
-// colorItem.style.backgroundColor = "blue";
 const form = document.querySelector('form');
 const bookInfo = document.querySelector('.all-books');
 const title = document.querySelector('#input-title');
